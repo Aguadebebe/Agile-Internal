d1e69db2930235f4104d06d9a7d8beef6ebcaa2a
Aguadebebe <81484212+Aguadebebe@users.noreply.github.com>
2023-06-05 12:31:03 -0400

    Initial Commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
     <AppBar position="static" sx={{ background: `linear-gradient(to right, #0000FF,   #00FF00)` }}>
       <Toolbar>
         <Typography className="pr-5" variant="h6" component={Link} to="/" sx={{ textDecoration: 'none', color: 'inherit', '&:hover': {
-          color: '#FFFFFF', // Customize the hover color
+          color: '#FFFFFF', // Customize the hover color so when Mouse hovers over link it changes to white
         }, }}>
           HomePage
         </Typography>
