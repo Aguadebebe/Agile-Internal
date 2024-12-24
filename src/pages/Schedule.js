cbaea7a15bcc1b8aecef8cdbc86e2ed21a911de6
Aguadebebe <81484212+Aguadebebe@users.noreply.github.com>
2024-12-24 07:04:01 -0500

    css accident, hopefully fixed

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-/*import '../App.css';*/
+import '../App.css';
 import Catalina_Sunset from "../images/Catalina_Sunset.png";
 
 function Schedule() {
